25d3863562bc385f7e289f0e28c5468d9be64e28
torodavid <torodavid00@gmail.com>
2018-08-20 11:20:00 +0200

    kisnyul

---
+++ src/main/java/com/torodavid/thesis/financialregister/FinancialRegisterApplication.java
@@ -0,0 +1,12 @@
+package com.torodavid.thesis.financialregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialRegisterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/torodavid/thesis/financialregister/FinancialRegisterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.torodavid.thesis.financialregister;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FinancialRegisterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
