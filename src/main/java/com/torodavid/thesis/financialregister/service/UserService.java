906b57dfe866e50109e4b4cfa1183ee13ba17da7
torodavid <torodavid00@gmail.com>
2018-08-22 22:26:35 +0200

    scope correction

--- src/main/java/com/torodavid/thesis/financialregister/service/UserService.java
+++ src/main/java/com/torodavid/thesis/financialregister/service/UserService.java
@@ -1,21 +1,8 @@
 package com.torodavid.thesis.financialregister.service;
 
-import com.torodavid.thesis.financialregister.dal.dao.CashFlow;
-import com.torodavid.thesis.financialregister.dal.dto.StatisticsWrapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-
-    @Autowired
-    CashFlowService cashFlowService;
-
-    public StatisticsWrapper csudalatosStatisztikaiKisnyulakParameterekbol(Long... ids) {
-        Iterable<CashFlow> allCashFlowsByIds = cashFlowService.getAllCashFlowsByIds(ids);
-
-
-        return new StatisticsWrapper();
-    }
 
 }
